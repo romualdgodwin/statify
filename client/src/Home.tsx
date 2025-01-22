825c20823f9f2d7f86184055d47a0fe25dd56cb6
Michaël Duthilleul <michael.duthilleul@gmail.com>
2025-01-22 16:38:15 +0100

    [client] Add go to plop link

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./Home.css";
 import { Page } from "./Page";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   const [count, setCount] = useState(0);
@@ -11,6 +12,9 @@ export const Home = () => {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Link to="plop">
+          <span>Go to Plop</span>
+        </Link>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
