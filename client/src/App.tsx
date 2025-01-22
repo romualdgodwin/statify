ce729479e3c17ff1b1bcc06677e42f5a14c7fa90
Michaël Duthilleul <michael.duthilleul@gmail.com>
2025-01-22 16:06:52 +0100

    [client] Use react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,12 @@
-import { useState } from "react";
-import "./App.css";
-import { Header } from "./Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./Home";
 
 export const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <Header title="Plop" />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
