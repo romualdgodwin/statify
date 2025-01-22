c1cbfa02748c85b0a01ffed509f1bb1725a1229c
Michaël Duthilleul <michael.duthilleul@gmail.com>
2025-01-22 17:11:05 +0100

    Add bootstrap

--- client/src/Plop.tsx
+++ client/src/Plop.tsx
@@ -1,3 +1,4 @@
+import { Button } from "react-bootstrap";
 import "./Home.css";
 import { Page } from "./Page";
 
@@ -5,6 +6,11 @@ export const Plop = () => {
   return (
     <Page title="Plop">
       <div className="card">Coucou !</div>
+      <button>Without bootsrap</button>
+      <button type="button" className="btn btn-danger">
+        With bootstrap
+      </button>
+      <Button variant="warning">Plop</Button>
     </Page>
   );
 };
