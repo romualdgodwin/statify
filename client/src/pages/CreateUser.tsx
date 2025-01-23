a191a9a670c03995ef7a07c944c05daa3c568c1e
Michaël Duthilleul <michael.duthilleul@gmail.com>
2025-01-23 16:36:50 +0100

    [client] Use react-hook-form

--- client/src/pages/CreateUser.tsx
+++ client/src/pages/CreateUser.tsx
@@ -1,54 +1,46 @@
 import { Button, Form } from "react-bootstrap";
 import { Page } from "../components/Page";
-import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+type UserForm = {
+  login: string;
+  password: string;
+  isAdmin: boolean;
+};
 
 export const CreateUser = () => {
-  const [login, setLogin] = useState("");
-  const [password, setPassword] = useState("");
-  const [isAdmin, setIsAdmin] = useState(false);
+  const { register, handleSubmit } = useForm<UserForm>();
 
   const navigate = useNavigate();
 
+  const onSubmit = handleSubmit(async ({ isAdmin, login, password }) => {
+    await axios.post("http://localhost:3000/users", {
+      login,
+      password,
+      role: isAdmin ? "admin" : "user",
+    });
+    navigate("/users");
+  });
+
   return (
     <Page title="Create User">
-      <Form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          await axios.post("http://localhost:3000/users", {
-            login,
-            password,
-            role: isAdmin ? "admin" : "user",
-          });
-          navigate("/users");
-        }}
-      >
+      <Form onSubmit={onSubmit}>
         <Form.Group>
           <Form.Label>Login</Form.Label>
-          <Form.Control
-            placeholder="Enter login"
-            value={login}
-            onChange={(e) => setLogin(e.target.value)}
-          />
-          <span>Value is {login}</span>
+          <Form.Control placeholder="Enter login" {...register("login")} />
         </Form.Group>
         <Form.Group>
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
             placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            {...register("password")}
           />
         </Form.Group>
         <Form.Group>
-          <Form.Check
-            type="checkbox"
-            label="Admin"
-            checked={isAdmin}
-            onChange={(e) => setIsAdmin(e.target.checked)}
-          />
+          <Form.Check type="checkbox" label="Admin" {...register("isAdmin")} />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
