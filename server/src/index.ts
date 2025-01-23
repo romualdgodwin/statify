1d45cb2f00fb406c92fe1d331fe31ae601dba8fc
Michaël Duthilleul <michael.duthilleul@gmail.com>
2025-01-23 09:46:33 +0100

    [server] Add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,14 +2,14 @@ import express from 'express'
 import { AppDataSource } from './dataSource'
 import { userController } from './modules/user/userController'
 import { authController } from './modules/auth/authController'
+import cors from 'cors'
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Plop!')
 })
-
 app.use('/users', userController)
 app.use('/auth', authController)
 
