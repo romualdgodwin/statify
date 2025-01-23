83d1f8b7efd45fb9259cd6de8109f2a282e2ddb1
Michaël Duthilleul <michael.duthilleul@gmail.com>
2025-01-23 11:43:41 +0100

    [client] add loading state to Users page

--- client/src/Users.tsx
+++ client/src/Users.tsx
@@ -4,22 +4,31 @@ import { useEffect, useState } from "react";
 
 export const Users = () => {
   const [users, setUsers] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const data = (await axios.get("http://localhost:3000/users")).data;
-      setUsers(data);
+      try {
+        const data = (await axios.get("http://localhost:3000/users")).data;
+        setUsers(data);
+      } finally {
+        setTimeout(() => setIsLoading(false), 1000);
+      }
     };
     fetchUsers();
   }, []);
 
   return (
     <Page title="Users">
-      <ul>
-        {users.map((user: any) => (
-          <li key={user.id}>{user.login}</li>
-        ))}
-      </ul>
+      {isLoading ? (
+        <span>Loading...</span>
+      ) : (
+        <ul>
+          {users.map((user: any) => (
+            <li key={user.id}>{user.login}</li>
+          ))}
+        </ul>
+      )}
     </Page>
   );
 };
