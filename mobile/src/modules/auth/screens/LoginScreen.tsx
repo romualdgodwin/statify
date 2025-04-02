899caa9db7169a832295760399c327604a6e3158
Michaël Duthilleul <michael.duthilleul@gmail.com>
2025-04-02 11:03:47 +0200

    [mobile] Add scrollview to login page

--- mobile/src/modules/auth/screens/LoginScreen.tsx
+++ mobile/src/modules/auth/screens/LoginScreen.tsx
@@ -1,48 +1,85 @@
 import { router, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useRef, useState } from "react";
+import {
+  Alert,
+  Button,
+  KeyboardAvoidingView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export const LoginScreen = () => {
   const [value, setValue] = useState("");
+
+  const scrollViewRef = useRef<ScrollView>(null);
+
   return (
-    <View style={styles.container}>
-      <Stack.Screen options={{
-        title: "Login"
-      }} />
-      <TextInput
-        style={styles.input}
-        value={value}
-        onChangeText={setValue}
-      />
-      <View style={styles.button}>
-        <Button title="Go" onPress={() => {
-          router.push("/home");
-        }} />
+    <ScrollView ref={scrollViewRef} style={{flex: 1}} contentContainerStyle={{ paddingBottom: 300}}>
+      <View style={styles.container}>
+        <Stack.Screen
+          options={{
+            title: "Login",
+          }}
+        />
+        <View style={styles.topView}>
+          <Text>Image</Text>
+        </View>
+        <TextInput
+          style={styles.input}
+          value={value}
+          onChangeText={setValue}
+          placeholder="Login"
+          onFocus={() => {
+            console.log('scroll')
+            scrollViewRef.current?.scrollToEnd()
+          }}
+        />
+        <TextInput
+          style={styles.input}
+          value={value}
+          onChangeText={setValue}
+          placeholder="Password"
+        />
+        <View style={styles.button}>
+          <Button
+            title="Submit"
+            onPress={() => {
+              router.push("/home");
+            }}
+          />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row",
     padding: 16,
     gap: 16,
   },
   input: {
     borderWidth: 1,
     borderRadius: 10,
-    flex: 1,
     padding: 16,
+  },
+  topView: {
+    height: 500,
+    backgroundColor: "#00000055",
+    borderColor: "black",
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
   button: {
     borderWidth: 1,
-    borderRadius: "100%",
+    borderRadius: 50,
     padding: 16,
   },
 });
