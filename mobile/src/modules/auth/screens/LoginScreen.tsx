60ad8ce6b2d163f4e3d791778fbd8fd97b195590
Michaël Duthilleul <michael.duthilleul@gmail.com>
2025-04-02 15:23:00 +0200

    [mobile] Add Firestore database

--- mobile/src/modules/auth/screens/LoginScreen.tsx
+++ mobile/src/modules/auth/screens/LoginScreen.tsx
@@ -1,5 +1,6 @@
 import { router, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { useRef, useState } from "react";
 import {
   Alert,
@@ -12,14 +13,27 @@ import {
   TextInput,
   View,
 } from "react-native";
+import { firestore } from "../../utils/firebase";
 
 const logo = require('../resources/ort-logo.png')
 
 export const LoginScreen = () => {
-  const [value, setValue] = useState("");
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
 
   const scrollViewRef = useRef<ScrollView>(null);
   const passwordRef = useRef<TextInput>(null);
+
+  const onSubmit = async () => {
+    const usersRef = collection(firestore, 'users')
+    const usersQuery = query(usersRef, where('login', '==', login), where('password', '==', password))
+    const docs  = await getDocs(usersQuery)
+    if(docs.empty) {
+      Alert.alert("Login failed", "Invalid credentials");
+      return;
+    }
+    router.push("/home");
+  }
 
   return (
     <ScrollView ref={scrollViewRef} style={{flex: 1}} contentContainerStyle={{ paddingBottom: 300}}>
@@ -31,9 +45,13 @@ export const LoginScreen = () => {
         />
         <Image style={styles.image} source={logo} />
         <TextInput
+         autoCapitalize="none"
+          autoCorrect={false}
+          autoComplete="username"
+          textContentType="username"
           style={styles.input}
-          value={value}
-          onChangeText={setValue}
+          value={login}
+          onChangeText={setLogin}
           placeholder="Login"
           onFocus={() => {
             console.log('scroll')
@@ -46,18 +64,17 @@ export const LoginScreen = () => {
         />
         <TextInput
          ref={passwordRef}
+         secureTextEntry
           style={styles.input}
-          value={value}
-          onChangeText={setValue}
+          value={password}
+          onChangeText={setPassword}
           placeholder="Password"
           returnKeyType="send"
         />
         <View style={styles.button}>
           <Button
             title="Submit"
-            onPress={() => {
-              router.push("/home");
-            }}
+            onPress={onSubmit}
           />
         </View>
       </View>
