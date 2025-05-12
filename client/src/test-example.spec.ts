5392b9ce1df644c14e28414ad2dcc41617b8abcb
Michaël Duthilleul <michael.duthilleul@gmail.com>
2025-05-12 09:54:30 +0200

    [client] Add tests with mock

--- client/src/test-example.spec.ts
+++ client/src/test-example.spec.ts
@@ -1,8 +1,23 @@
-import { myFunctionToTest } from "./test-example";
+import { filterUsers, myFunctionToTest } from "./test-example";
 
 describe("Example test", () => {
   test("It returns a + b", () => {
     const result = myFunctionToTest(42, 23);
     expect(result).toEqual(65);
+  });
+
+  test("Filter users", () => {
+    const users = filterUsers((user) => user.age > 25);
+
+    expect(users.length).toEqual(1);
+    expect(users[0].name).toEqual("John");
+  });
+
+  test("Test with mock", () => {
+    const mock = jest.fn(() => true);
+    const users = filterUsers(mock);
+    expect(users.length).toEqual(2);
+    expect(mock).toHaveBeenCalledTimes(2);
+    expect(mock).toHaveBeenNthCalledWith(1, { name: "John", age: 30 });
   });
 });
--- client/src/test-example.ts
+++ client/src/test-example.ts
@@ -1,3 +1,16 @@
 export const myFunctionToTest = (a: number, b: number): number => {
   return a + b;
 };
+
+type User = {
+  name: string;
+  age: number;
+};
+
+const users = [
+  { name: "John", age: 30 },
+  { name: "Jane", age: 25 },
+];
+
+export const filterUsers = (filter: (user: User) => boolean): User[] =>
+  users.filter(filter);
