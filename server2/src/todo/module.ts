c402dadb8cfabec48fe9532e0db6617e1d2ede7e
Michaël Duthilleul <michael.duthilleul@gmail.com>
2025-05-14 11:59:48 +0200

    [server2] Add deno/danet example

---
+++ server2/src/todo/module.ts
@@ -0,0 +1,14 @@
+import { TodoController } from './controller.ts';
+import { TodoService } from './service.ts';
+import { Module, TokenInjector } from '@danet/core';
+import { USER_REPOSITORY } from './constant.ts';
+import { PostgresRepository } from "./postgres-repository.ts";
+import { DatabaseModule } from "../database/module.ts";
+import { TodoViewController } from './view.controller.ts';
+
+@Module({
+  controllers: [TodoController, TodoViewController],
+  injectables: [new TokenInjector(PostgresRepository, USER_REPOSITORY), TodoService], // change InMemoryTodoRepository by any custom repository using other database engine if needed
+    imports: [DatabaseModule]
+})
+export class TodoModule {}
